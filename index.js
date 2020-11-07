775b26882fe4c7151e1629bb8a796a446ae3964d
nii-kwartei <38301860+nii-kwartei@users.noreply.github.com>
2020-11-07 11:51:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// Generate random numbers for selecting a random dice face
+var randomNumber1  = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice2.png
+
+var randomImageSource = "images/" + randomDiceImage;//images/dice1.png - /images/dice2.png
+
+var image1 = document.querySelectorAll("img")[0];// selecting img class
+
+image1.setAttribute("src", randomImageSource);
+
+
+// Generate random numbers for player2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; // 1
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+// if player 1 wins
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
